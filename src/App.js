3be0dfe787f8ecdcc55104b7cbcad0e9bb2622ec
abdullah60902 <butt60902@gmail.com>
2024-02-27 17:25:21 +0500

    asg 1

---
+++ src/App.js
@@ -0,0 +1,160 @@
+import logo from './logo.svg';
+import './App.css';
+import img1 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.14 PM (1).jpeg"
+import img2 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.12 PM.jpeg"
+import img3 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.14 PM.jpeg"
+import img4 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.14 PM (4).jpeg"
+import img5 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.14 PM (2).jpeg"
+import img6 from "../src/asset/WhatsApp Image 2023-10-12 at 8.28.14 PM (3).jpeg"
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        <img src={img1} alt="" className='borderimg' />
+      </div>
+      <div className='center'>
+        <h3 >Ara Profile
+        </h3>
+      </div>
+      <div className='dd2'>
+
+        <div className='paradiv'>
+          <p>profile sheet 0.40 mm to 1.0 mm 28 gauge into 20 gauge and worth 100 mm(39 ,,) X 1220 mm(48 ,,)1500 mm(50 ,,)and send where
+
+          </p>
+          <p className='you'>you want.Gi sheet and Roll,Coated Sheet and Roll, H.R.C Sheet and Roll and Stain-less steel also can profile by us
+
+          </p>
+        </div>
+        <div>
+          <div className='imgdisplay'>
+            <img src={img2} alt="" className='imgst' />
+            <img src={img3} alt="" className='imgst' />
+            <img src={img4} alt="" className='imgst' />
+
+          </div>
+          <div className='db'>
+
+            <a href="https://www.google.com/">fb.com/naveed.trainer</a>
+
+          </div>
+
+        </div>
+
+      </div>
+      <div className='mainm'>
+
+        <div>
+          <img src={img5} alt="" className='img5' />
+        </div>
+
+        <div className='paramain'>
+          <h3 className='headst'>Cable Tray
+          </h3>
+          <p className='para'>We have many diffrnt kind and size of cable Tray in M/S.Galvanize and coated sheet.you can purchase S.S Cable Tray from us.cable Tray of any industries you can buy from us.</p>
+          <p className='para'>We have many diffrnt kind and size of cable Tray in M/S.Galvanize and coated sheet.you can purchase S.S Cable Tray from us.cable Tray of any industries you can buy from us.</p>
+          <p className='para'>We have many diffrnt kind and size of cable Tray in M/S.Galvanize and coated sheet.you can purchase S.S Cable Tray from us.cable Tray of any industries you can buy from us.</p>
+
+        </div>
+
+
+
+<div className='lm'>
+  <div className='po'>
+<img src={img6} alt="" className='img6' />
+</div>
+
+  <div className='down'>
+<p>C-Channels made by us in every guages. 3.00,
+   </p> 
+
+
+<p>2.50, 2.00, 1.80, 1.60,length 10 feet We can make  in </p>
+<p className='pp'> different  sizes in shapes and we can also make  C-Channels  for petrol pumps.CNG stations    
+and tool plazas.</p>
+
+  </div>
+</div>
+
+
+
+
+
+
+
+
+      </div>
+
+
+<div className='mainul'>
+<div className='mrleft'>
+  <ul>
+    <li>Ara</li>
+    <li>Profile</li>
+    <li>Sheet</li>
+    <li>170</li>
+  </ul>
+</div>
+
+<div className='mrleft2'>
+  <ul>
+    <li>Ara</li>
+    <li>Profile</li>
+    <li>Sheet</li>
+    <li>170</li>
+  </ul>
+</div>
+
+<div className='ol'>
+  <ol>
+    <li>Ara</li>
+    <li>Profile</li>
+    <li>Sheet</li>
+    <li>170</li>
+  </ol>
+</div>
+
+
+
+
+</div>
+
+
+<div>
+<h1 className='ce'>Join Us
+</h1>
+<form action="" className='c'>
+<input type="text" placeholder='Name' className='input0' />
+<input type="address" placeholder='Address'  className='input0 mrinput3'  />
+<input type="text" placeholder='City'  className='input0' />
+<input type="text" placeholder='STATE'  />
+<input type="text" placeholder='ZIP'  className='mrinput2' />
+<select name="Newswork" id="Newswork" className='mrinput'>
+<option value="Newswork">Newswork</option>
+<option value="Ara">Ara</option>
+<option value="Profile">Profile</option>
+<option value="Cable">Cable</option>
+
+
+</select>
+
+<div className='rty'>
+<input type="radio" name="im" id="bro" /><p className='year'>1 Year</p>
+<input type="radio" name="im" id="bro"  className='bro'/><p className='year'>2 Year</p>
+</div>
+<textarea name="comment" id="comment" cols="30" rows="5" placeholder='comment...'></textarea>
+
+</form>
+
+
+</div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
